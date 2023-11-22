85b9ebc82320a7c6a2d79c32c82734a133474088
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2023-11-22 09:09:57 -0300

    :sparkles: [feat] Autenticação na documentação

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ from Routes.r_clientes import clientes
 
 app = Flask(__name__)
 
+authorizations = {
+    "bearerAuth": {
+        "type": "apiKey",
+        "scheme": "bearer",
+        "bearerFormat": "JWT",
+        "in": "header",
+        "name": "Authorization"
+    }
+}
+
 api = Api(app, version="1.0",
           title="Documentação da API do sistema de ativos MoreU",
-          description="API desenvolvida para realização da manipulação de dados do sistema de ativos MoreU, coletando dados enviados do frontend por meio de requisições e caminhos de URL, tratando eles no backend e inserindo/buscando/atualizando na base de dados e, por fim, entregando-os ao frontend novamente.", doc="/doc")
+          description="API desenvolvida para realização da manipulação de dados do sistema de ativos MoreU, coletando dados enviados do frontend por meio de requisições e caminhos de URL, tratando eles no backend e inserindo/buscando/atualizando na base de dados e, por fim, entregando-os ao frontend novamente.",
+          authorizations=authorizations,
+          security="http",  
+          doc="/doc")
 CORS(app, expose_headers="Authorization")
 
 app.config['JSON_SORT_KEYS'] = False
